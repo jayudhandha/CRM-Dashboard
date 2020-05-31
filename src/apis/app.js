770925602dac87c7fdb99514de66e1904ad3d9ae
jayudhandha <jayudhandha@gmail.com>
2020-05-31 11:17:54 +0530

    POST API and Get specific student object from Api

--- src/apis/app.js
+++ src/apis/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
+var bodyParser = require('body-parser')
 const exApp = express()
+
+// create application/json parser
+var jsonParser = bodyParser.json()
 
 // Middleware
 exApp.use((req, res, next) => {
@@ -17,16 +20,6 @@ exApp.get('/api/student', (req, res) => {
     res.status(200).send(students);
 })
 
-// exApp.get('/api/student/{:id}', (req, res) => {
-//   students = [
-//     {id: "1", name:"Bhaumik", branch: "ICT"},
-//     {id: "2", name:"Rutvik", branch: "ICT"},
-//     {id: "3", name:"Nipun", branch: "AWS"},
-//   ];
-//   let studentId = id
-//   res.status(200).send(student[studentId]);
-// })
-
 exApp.get('/api/faculty', (req, res) => {
   faculties = [
     {id: "1", name:"CDP Sir", branch: "ICT"},
@@ -34,6 +27,24 @@ exApp.get('/api/faculty', (req, res) => {
     {id: "3", name:"Kartik Sir", branch: "ICT"},
   ];
   res.status(200).send(faculties);
+})
+
+exApp.get('/api/student/:id', (req, res) => {
+  students = [
+    {id: "1", name:"Bhaumik", branch: "ICT"},
+    {id: "2", name:"Rutvik", branch: "ICT"},
+    {id: "3", name:"Nipun", branch: "AWS"},
+  ];
+  console.log("Passed Id is: " + req.params.id);
+  const foundStudent = students.filter(student =>
+    student.id == req.params.id
+  );
+  res.status(200).send(foundStudent);
+})
+
+exApp.post('/api/student', jsonParser, (req, res) => {
+  console.log('Passed body : ' + JSON.stringify(req.body));
+  res.status(200).send(req.body)
 })
 
 module.exports = exApp
