71dfd5e2c27d4734c938bd1edf4439b3056cf5aa
Jayesh Dhandha <jayesh.dhandha@adrixus.com>
2020-07-26 13:05:35 +0530

    changed base path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_BASE_PATH: 'https://student-crm-backend.herokuapp.com/api/',
-  AUTH_BASE_PATH: 'https://student-crm-backend.herokuapp.com/auth/'
+  API_BASE_PATH: 'https://mu-backend.herokuapp.com/api/',
+  AUTH_BASE_PATH: 'https://mu-backend.herokuapp.com/auth/'
 };
